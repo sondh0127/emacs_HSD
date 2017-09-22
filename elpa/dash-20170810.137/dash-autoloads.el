eeaf7fd29563538e497aee9408dd14a10d2302f8
Do Hong Son <sonstephendo@gmail.com>
2017-09-22 22:04:31 +0700

    remove stuff

--- elpa/dash-20170810.137/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("dash.el") (22963 47151 804049 710000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
