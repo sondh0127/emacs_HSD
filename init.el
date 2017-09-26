d29b16cfa1acfb5d8ea4f4d16c2b4968195440e0
Do Hong Son <sonstephendo@gmail.com>
2017-09-26 12:28:02 +0700

    modified:   init.el 	modified:   myinit.org

--- init.el
+++ init.el
@@ -11,4 +11,19 @@
 	(package-install 'use-package))
 (require 'diminish)
 (require 'bind-key)
+(setq use-package-always-ensure t)
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (treemacs yasnippet which-key use-package try test-simple tabbar sr-speedbar smooth-scrolling smartparens powerline pfuture org-plus-contrib org-bullets multiple-cursors moe-theme loc-changes load-relative hydra hungry-delete ggtags flycheck-irony f expand-region evil counsel company-irony company-c-headers color-theme beacon auto-complete aggressive-indent ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
