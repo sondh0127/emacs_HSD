f41aeca2497399b40dd843d787854d3706991a5e
Do Hong Son <sonstephendo@gmail.com>
2017-09-30 14:57:54 +0700

    test config

--- init.el
+++ init.el
@@ -20,7 +20,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (treemacs yasnippet which-key use-package try test-simple tabbar sr-speedbar smooth-scrolling smartparens powerline pfuture org-plus-contrib org-bullets multiple-cursors moe-theme loc-changes load-relative hydra hungry-delete ggtags flycheck-irony f expand-region evil counsel company-irony company-c-headers color-theme beacon auto-complete aggressive-indent ace-window))))
+	(smartparens-config treemacs yasnippet which-key use-package try test-simple tabbar sr-speedbar smooth-scrolling smartparens powerline pfuture org-plus-contrib org-bullets multiple-cursors moe-theme loc-changes load-relative hydra hungry-delete ggtags flycheck-irony f expand-region evil counsel company-irony company-c-headers color-theme beacon auto-complete aggressive-indent ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
